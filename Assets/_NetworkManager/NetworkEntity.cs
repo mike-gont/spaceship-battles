9ac5d145cea9c8b1176c7df56451bdf8f18c9287
Kir <yakirgont@gmail.com>
2018-03-27 18:24:04 +0300

    NetworkEntity created

---
+++ Assets/_NetworkManager/NetworkEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkEntity : MonoBehaviour {
+
+    Queue<NetworkMessage> incomingQueue = new Queue<NetworkMessage>();
+
+
+  
+}
