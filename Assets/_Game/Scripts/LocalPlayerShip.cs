901b2aebf53b3b0f952aca256fe583ed4d727617
Mike Gont <mike.gont@gmail.com>
2018-03-30 15:18:33 +0300

    starting to work on PlayerShip classes

--- Assets/_Game/Scripts/LocalPlayerShip.cs
+++ Assets/_Game/Scripts/LocalPlayerShip.cs
@@ -14,16 +14,7 @@ public class LocalPlayerShip : PlayerShip {
         Vector3 linear_input = new Vector3(input.strafe, 0.0f, input.throttle);
         Vector3 angular_input = new Vector3(input.pitch, input.yaw, input.roll);
 
-        if (!isServer) {
-            physics.SetPhysicsInput(linear_input, angular_input);
-            //networkController.GetComponent<Client>().SendHost(linear_input, angular_input);
-        }
-        else {
-            physics.SetPhysicsInput(linear_input_sent, angular_input_sent);
-        }
-
-        if (isPlayer)
-            activeShip = this;
+        physics.SetPhysicsInput(linear_input, angular_input);
 
         // shooting
         if ((Input.GetButton("RightTrigger") || Input.GetMouseButtonDown(0)) && Time.time > nextFire) {
@@ -31,5 +22,8 @@ public class LocalPlayerShip : PlayerShip {
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
             GetComponent<AudioSource>().Play();
         }
+
+        if (isPlayer)
+            activeShip = this;
     }
 }
--- Assets/_Game/Scripts/RemotePlayerShip.cs
+++ Assets/_Game/Scripts/RemotePlayerShip.cs
@@ -6,5 +6,12 @@ public class RemotePlayerShip : PlayerShip {
 
     private void Update() {
 
+        if (!isServer) {
+
+            //networkController.GetComponent<Client>().SendHost(linear_input, angular_input);
+        }
+        else {
+            physics.SetPhysicsInput(linear_input_sent, angular_input_sent);
+        }
     }
 }
