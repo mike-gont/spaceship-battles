cdfa3e31a2e206328931c8e6d91d747eea766bb4
Kir <yakirgont@gmail.com>
2018-04-05 15:28:30 +0300

    projectile

--- Assets/_Game/Scripts/Projectile.cs
+++ Assets/_Game/Scripts/Projectile.cs
@@ -6,15 +6,44 @@ public class Projectile : NetworkEntity {
     public GameObject projExplosion;
     public float timeout = 10.0f;
 
+    private bool IsServer = false;
+
+    public static float LERP_MUL = 3f;
+
     void Start ()
     {
-        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+        GameObject networkController = GameObject.Find("ClientNetworkController");
+        if (networkController == null)
+            IsServer = true;
+
+        if (IsServer)
+            GetComponent<Rigidbody>().velocity = transform.forward * speed;
     }
 	
 	void Update ()
     {
-		
-	}
+        if (IsServer)
+            return;
+        if (incomingQueue.Count == 0)
+            return;
+        NetMsg netMessage = incomingQueue.Dequeue();
+        switch (netMessage.Type) {
+            case (byte)NetMsg.MsgType.SC_MovementData:
+                MoveProjUsingReceivedServerData((SC_MovementData)netMessage);
+                break;
+            case (byte)NetMsg.MsgType.SC_EntityDestroyed:
+                Destroy(gameObject);
+                break;
+            default:
+                Debug.Log("ERROR! RemoteProjectile on Client reveived an invalid NetMsg message. NetMsg Type: " + netMessage.Type);
+                break;
+        }
+    }
+
+    private void MoveProjUsingReceivedServerData(SC_MovementData message) {
+        transform.position = Vector3.Lerp(transform.position, message.Position, LERP_MUL * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, message.Rotation, LERP_MUL * Time.deltaTime);
+    }
 
     private void Awake()
     {
